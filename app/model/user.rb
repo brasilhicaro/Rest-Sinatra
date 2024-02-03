827263f46ab9bbaafa3ee08c0b0d4934b05e27e3
Hicaro_Brasil <hicaro.brasil@hotmail.com>
2024-02-03 08:59:39 -0300

    :sparkles: feat: user model

---
+++ app/model/user.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'secure_random'
+
+class User
+  attr_accessor :id, :name
+
+  def initialize(name:)
+    @id = SecureRandom.uuid
+    @name = name
+  end
+end
